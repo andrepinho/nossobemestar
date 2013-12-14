c342234f106146e68a9275c9ca8467687bb32d83
andrepinho <andresouzapinho@gmail.com>
2013-12-14 04:57:40 -0200

    Change email notice again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,6 +22,6 @@ class HomeController < ApplicationController
     end
   end
   def email
-    redirect_to root_path, notice: 'Obrigado por seu cadastro. Para concluir, enviamos a confirmação para o seu email.'
+    redirect_to root_path, notice: 'Obrigado por seu cadastro. Por favor, confirme a sua inscrição em seu email.'
   end
 end
