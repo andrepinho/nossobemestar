370ad44ed563765807b341bc68677c1e33e44015
Daniel Weinmann <danielweinmann@gmail.com>
2014-04-28 11:19:09 -0300

    Adding missing files for facebook login

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,7 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user, :event => :authentication
+    set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+  end
+end
