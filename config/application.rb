28b4fcbfdb3c67c7cdbdffae7d161aa17e53e73a
André Pinho <andresouzapinho@gmail.com>
2021-03-01 18:38:35 -0300

    Precompile ckeditor assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(:default, Rails.env)
 module Portal
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
