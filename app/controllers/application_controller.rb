c99d2f29ab0d712e54855890954bf276d0005bc2
André Pinho <andresouzapinho@gmail.com>
2022-01-15 13:50:00 -0300

    Remove forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_section instead.
-  protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :remove_subdomain
   before_filter :detect_region
