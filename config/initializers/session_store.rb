ba769309c75d25ba8832f3f16d64b14985833a5c
André Pinho <andresouzapinho@gmail.com>
2022-01-15 14:46:32 -0300

    Fix typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Portal::Application.config.session_store :cookie_store, key: '_Portal_session', domain: "arquvinossobemestar.com"
+  Portal::Application.config.session_store :cookie_store, key: '_Portal_session', domain: "arquivonossobemestar.com"
 else
   Portal::Application.config.session_store :cookie_store, key: '_Portal_session', domain: "lvh.me"
 end
